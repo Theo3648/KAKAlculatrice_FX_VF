b4bebd6d46442dc38d366143650632f1db40294c
Théo <Tho@Theo_PCPortable>
2024-10-27 21:54:31 +0100

    JE CROIS  BIEN QUE CA FONCTIONNE ! YOUHOU !

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,6 +1,8 @@
 package view;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -11,11 +13,17 @@ public class Main extends Application {
         // Créer une disposition (layout)
         StackPane root = new StackPane();
 
+        // Créer un label avec le texte souhaité
+        Label messageLabel = new Label("Je crois bien que ça fonctionne ! YOUHOU !");
+
+        // Ajouter le label à la disposition
+        root.getChildren().add(messageLabel);
+
         // Créer une scène avec la disposition et définir sa taille
         Scene scene = new Scene(root, 400, 300);
 
         // Définir le titre de la fenêtre
-        primaryStage.setTitle("Fenêtre Vierge");
+        primaryStage.setTitle("Fenêtre avec message");
 
         // Ajouter la scène à la fenêtre principale
         primaryStage.setScene(scene);
